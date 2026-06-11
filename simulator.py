2616b7a36073a8ec9e846ab5bad5bf0051c74760
lahiru <lahiru@theavo.net>
2026-06-11 16:17:32 +0530

    axx

---
+++ simulator.py
@@ -0,0 +1,17 @@
+from pymodbus.server import StartTcpServer
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.datastore import ModbusSequentialDataBlock
+
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# Create datastore with 100 coils all set to 0
+store = ModbusSlaveContext(
+    co=ModbusSequentialDataBlock(0, [0]*100)
+)
+context = ModbusServerContext(slaves=store, single=True)
+
+print(">>> Modbus Simulator running on port 5020...")
+StartTcpServer(context=context, address=("0.0.0.0", 5020))
