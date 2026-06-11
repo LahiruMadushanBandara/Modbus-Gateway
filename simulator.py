cdeb5919e6377fb42137dabe12477c86ee79003b
lahiru <lahiru@theavo.net>
2026-06-11 16:37:32 +0530

    zz

--- simulator.py
+++ simulator.py
@@ -1,18 +1,22 @@
 from pymodbus.server import StartTcpServer
-from pymodbus.datastore import ModbusSlaveContext
-from pymodbus.datastore import ModbusServerContext
-from pymodbus.datastore import ModbusSequentialDataBlock
-
-import logging
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+from pymodbus.datastore import (
+    ModbusSequentialDataBlock,
+    ModbusSlaveContext,
+    ModbusServerContext,
+)
 
 store = ModbusSlaveContext(
-    co=ModbusSequentialDataBlock(0, [0]*100)
+    di=ModbusSequentialDataBlock(0, [0]*100),
+    co=ModbusSequentialDataBlock(0, [0]*100),
+    hr=ModbusSequentialDataBlock(0, [0]*100),
+    ir=ModbusSequentialDataBlock(0, [0]*100),
 )
 
 context = ModbusServerContext(slaves=store, single=True)
 
 print(">>> Modbus Simulator running on port 5020...")
-StartTcpServer(context=context, address=("0.0.0.0", 5020))
+
+StartTcpServer(
+    context=context,
+    address=("0.0.0.0", 5020)
+)
